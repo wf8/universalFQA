f9d7de3ceecddbc59fca3650e42c08759365cf71
wf <willfreyman@gmail.com>
2013-07-17 12:34:05 -0700

    used jquery for existing ajax

--- js/fqa.js
+++ js/fqa.js
@@ -1,103 +1,71 @@
 /**
  * ---------------------------------------------------------
- * login/logout/register/change account functions
+ *
+ * login / register / change account functions
  *
  * ---------------------------------------------------------
  */
 function register() {
-	//setup new AJAX request 
-	var ajaxRequest  = new XMLHttpRequest();
-	// prepare POST parameters
-	var email = document.getElementById("register_email").value;
-	var last_name = document.getElementById("register_last_name").value;
-	var first_name = document.getElementById("register_first_name").value;
-	var password1 = document.getElementById("register_password1").value;
-	var password2 = document.getElementById("register_password2").value;
-	
-	var params = "email=" + email + "&password1=" + password1 + "&password2=" + password2 + "&last_name=" + last_name + "&first_name=" + first_name;
-	// send the new request 
-	var url = "../php/register_user.php";
-	ajaxRequest.onreadystatechange=function() {
-		if (ajaxRequest.readyState==4 && ajaxRequest.status==200) {
-			var response = ajaxRequest.responseText;
-			if (response.indexOf("success") != -1) {
+	$.ajax({
+		url: "../php/register_user.php",
+		type: "POST",
+		data: {
+			email: $("#register_email").val(),
+			first_name: $("#register_first_name").val(),
+			last_name: $("#register_last_name").val(),
+			password1: $("#register_password1").val(),
+			password2: $("#register_password2").val()
+		},
+		success: function( response ) {
+			if (response.indexOf("success") != -1) 
 				window.location='../php/assessments.php';
-			} else {
+			else 
 				alert(response);
-			}
 		}
-	}			
-	ajaxRequest.open("POST", url, true);				
-	// Send the proper header information along with the request 
-	ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	ajaxRequest.send(params);
+	});
 }
 
 function login() {
-	//setup new AJAX request 
-	var ajaxRequest  = new XMLHttpRequest();
-	// prepare POST parameters
-	var email = document.getElementById("login_email").value;
-	var login_password = document.getElementById("login_password").value;
-	var params = "email=" + email + "&password=" + login_password;
-	// send the new request 
-	var url = "../php/login_user.php";
-	ajaxRequest.onreadystatechange=function() {
-		if (ajaxRequest.readyState==4 && ajaxRequest.status==200) {
-			var response = ajaxRequest.responseText;
-			if (response.indexOf("success") != -1) {
+	$.ajax({
+		url: "../php/login_user.php",
+		type: "POST",
+		data: {
+			email: $("#login_email").val(),
+			password: $("#login_password").val(),
+		},
+		success: function( response ) {
+			if (response.indexOf("success") != -1) 
 				window.location='../php/assessments.php';
-			} else {
+			else 
 				alert(response);
-			}
 		}
-	}			
-	ajaxRequest.open("POST", url, true);				
-	// Send the proper header information along with the request 
-	ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	ajaxRequest.send(params);
+	});
 }
 
-function clear_login_forms() {
-	document.getElementById("register_email").value = "";
-	document.getElementById("register_last_name").value = "";
-	document.getElementById("register_first_name").value = "";
-	document.getElementById("register_password1").value = "";
-	document.getElementById("register_password2").value = "";
-	document.getElementById("login_email").value = "";
-	document.getElementById("login_password").value = "";
-}
 
 function save_account_changes() {
-	//setup new AJAX request 
-	var ajaxRequest  = new XMLHttpRequest();
-	// prepare POST parameters
-	var email = document.getElementById("change_email").value;
-	var last_name = document.getElementById("change_last_name").value;
-	var first_name = document.getElementById("change_first_name").value;
-	var password1 = document.getElementById("change_password1").value;
-	var password2 = document.getElementById("change_password2").value;
-	var params = "email=" + email + "&password1=" + password1 + "&password2=" + password2 + "&last_name=" + last_name + "&first_name=" + first_name;
-	// send the new request 
-	var url = "../php/change_user_info.php";
-	ajaxRequest.onreadystatechange=function() {
-		if (ajaxRequest.readyState==4 && ajaxRequest.status==200) {
-			var response = ajaxRequest.responseText;
-			if (response.indexOf("success") != -1) {
+	$.ajax({
+		url: "../php/change_user_info.php",
+		type: "POST",
+		data: {
+			email: $("#change_email").val(),
+			first_name: $("#change_first_name").val(),
+			last_name: $("#change_last_name").val(),
+			password1: $("#change_password1").val(),
+			password2: $("#change_password2").val()
+		},
+		success: function( response ) {
+			if (response.indexOf("success") != -1) 
 				alert("Changes saved!");
-			} else {
+			else 
 				alert(response);
-			}
 		}
-	}			
-	ajaxRequest.open("POST", url, true);				
-	// Send the proper header information along with the request 
-	ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	ajaxRequest.send(params);
+	});
 }
 
 /**
  * ---------------------------------------------------------
+ *
  * upload/download FQA databases functions
  *
  * ---------------------------------------------------------
@@ -108,11 +76,10 @@ function save_account_changes() {
 	return true;
 }
 function stop_database_upload( msg ){	
-	if ( msg.indexOf("Error:") != -1 ) {
+	if ( msg.indexOf("Error:") != -1 ) 
 		$( "#upload_error" ).html( msg );
-	} else {
+	else 
 		$( "#upload_error" ).html( "Data successfully uploaded." );
-	} 
 }
  
 function download_database() {
