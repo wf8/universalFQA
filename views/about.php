f2a7d62f33798e935181ba4468ea5164ac345024
wf <willfreyman@gmail.com>
2013-07-22 09:41:29 -0700

    fixed links in about

--- views/about.php
+++ views/about.php
@@ -1,7 +1,7 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
     	<div class="navbar-inner">
         	<div class="container">
-          		<a class="brand" href="index.html">Universal FQA</a>
+          		<a class="brand" href="../">Universal FQA</a>
           		<div class="nav-collapse collapse pull-right">
             		<ul class="nav">
             		</ul>
@@ -20,7 +20,7 @@
 					<h1>About this website</h1>
 					<br>
 					<p class="nice-text">
-						The <a href="index.html">Universal Floristic Quality Assessment Calculator</a> is an open source project licensed for use under <a href="http://www.gnu.org/licenses/">GPLv3</a>. It is completely free for any user, and is being developed by <a href="http://breadorpyramid.org">Will Freyman</a> with support from <a href="http://openlands.org">Openlands</a>. Questions or comments should be sent to willfreyman at gmail dot com.
+						The <a href="../">Universal Floristic Quality Assessment Calculator</a> is an open source project licensed for use under <a href="http://www.gnu.org/licenses/">GPLv3</a>. It is completely free for any user, and is being developed by <a href="http://breadorpyramid.org">Will Freyman</a> with support from <a href="http://openlands.org">Openlands</a>. Questions or comments should be sent to willfreyman at gmail dot com.
 					</p>
 					<p class="nice-text">
 						Photo of <em>Sisyrinchium angustifolium</em> &copy; <a href="http://www.flickr.com/photos/lc10s/">Lisa Culp</a>. Used with permission.
