912b1db570e9bd5161f46cd6434b1d596fe75b9d
wf <willfreyman@gmail.com>
2013-07-23 09:53:36 -0700

    made error view

---
+++ views/error.php
@@ -0,0 +1,30 @@
+    <div class="navbar navbar-inverse navbar-fixed-top">
+    	<div class="navbar-inner">
+        	<div class="container">
+          		<a class="brand" href="../">Universal FQA</a>
+          		<div class="nav-collapse collapse pull-right">
+            		<ul class="nav">
+            		</ul>
+          		</div>
+        	</div>
+      	</div>
+    </div>
+	<br>
+    <div class="container padding-top">
+		<div class="nice_margins">
+			<div class="row-fluid">
+				<div class="span3">
+					<img src="assets/images/blue-eyed.jpg" class="img-rounded">
+				</div>
+				<div class="span9">
+					<h1>Oops...</h1>
+					<br>
+					<p class="nice-text">
+						Something went wrong. 
+					</p>
+					<br>
+					<button class="btn btn-info" onclick="javascript: window.history.back();">Go back!</button>
+				</div>
+			</div>
+			<br><br><br><br><br><br><br><br><br>
+		</div>
