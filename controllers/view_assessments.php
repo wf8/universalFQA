0f611ba1778cb5235098001293556767a9ac2728
wf <willfreyman@gmail.com>
2013-08-05 19:51:14 -0700

    clear assessments from session

--- controllers/view_assessments.php
+++ controllers/view_assessments.php
@@ -3,6 +3,8 @@ if( !$_SESSION['valid'] )
 	require_once('../views/login.php');
 else {
 	require_once('../views/nav.php');
+	// clear any assessments out of the session
+	$_SESSION['assessment'] = null;
 	// get this user's inventory assessments
 	$inventory = new InventoryAssessment;
 	$inventory_assessments = $inventory->get_all_for_user($_SESSION['user_id']);
