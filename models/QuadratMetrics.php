883eab9830fd039d79e1efb09a30eaac74efbca4
wf <willfreyman@gmail.com>
2013-08-06 10:23:52 -0700

    moved some instance variables into protected properties

--- models/QuadratMetrics.php
+++ models/QuadratMetrics.php
@@ -63,6 +63,35 @@ class QuadratMetrics extends Metrics {
 	public $percent_native_perennial = 0;
 	public $percent_native_biennial = 0;
 
+	protected $total_c = 0;
+	protected $native_c = 0;
+	protected $total_w = 0;
+	protected $native_w = 0;
+		
+	protected $native_tree_c = 0;
+	protected $native_shrub_c = 0;
+	protected $native_herbaceous_c = 0;
+		
+	protected $c_0 = 0;
+	protected $c_1_3 = 0;
+	protected $c_4_6 = 0;
+	protected $c_7_10 = 0;
+		
+	protected $native_tree = 0;
+	protected $native_shrub = 0;
+	protected $native_herbaceous = 0;
+		
+	protected $total_coverage = 0;
+	protected $native_coverage = 0;
+	protected $sum_total_c_times_coverage = 0;	
+	protected $sum_native_c_times_coverage = 0;
+		
+	// checks to see if these data are in the taxon's fqa_db
+	// if no data set metrics to 'n/a'
+	protected $physiognomy = false;
+	protected $duration = false;
+	protected $wetness = false;
+
 	/*
 	 * constructor
 	 * takes as input an Quadrat object
@@ -72,79 +101,51 @@ class QuadratMetrics extends Metrics {
 		Metrics::__construct( $quadrat );
 		
 		$taxa = $quadrat->taxa;
-		$total_c = 0;
-		$native_c = 0;
-		$total_w = 0;
-		$native_w = 0;
-		
-		$native_tree_c = 0;
-		$native_shrub_c = 0;
-		$native_herbaceous_c = 0;
-		
-		$c_0 = 0;
-		$c_1_3 = 0;
-		$c_4_6 = 0;
-		$c_7_10 = 0;
-		
-		$native_tree = 0;
-		$native_shrub = 0;
-		$native_herbaceous = 0;
-		
-		$total_coverage = 0;
-		$native_coverage = 0;
-		$sum_total_c_times_coverage = 0;	
-		$sum_native_c_times_coverage = 0;
-		
-		// checks to see if these data are in the taxon's fqa_db
-		// if no data set metrics to 'n/a'
-		$physiognomy = false;
-		$duration = false;
-		$wetness = false;
 		
 		foreach( $taxa as $taxon ) {
 			$this->total_species++;
-			$total_c += $taxon->c_o_c;
-			$total_coverage += $taxon->percent_cover;
-			$sum_total_c_times_coverage += ($taxon->percent_cover * $taxon->c_o_c);
+			$this->total_c += $taxon->c_o_c;
+			$this->total_coverage += $taxon->percent_cover;
+			$this->sum_total_c_times_coverage += ($taxon->percent_cover * $taxon->c_o_c);
 			
 			if (trim($taxon->c_o_w) !== '') {
-				$wetness = true;
-				$total_w += $taxon->c_o_w;
+				$this->wetness = true;
+				$this->total_w += $taxon->c_o_w;
 			}
 			
 			if ($taxon->native == 'native') {
 				$this->native_species++;
-				$native_c += $taxon->c_o_c;
-				$native_w += $taxon->c_o_w;
-				$native_coverage += $taxon->percent_cover;
-				$sum_native_c_times_coverage += ($taxon->percent_cover * $taxon->c_o_c);
+				$this->native_c += $taxon->c_o_c;
+				$this->native_w += $taxon->c_o_w;
+				$this->native_coverage += $taxon->percent_cover;
+				$this->sum_native_c_times_coverage += ($taxon->percent_cover * $taxon->c_o_c);
 			}
 			
 			if ($taxon->c_o_c == 0)
-				$c_0++;
+				$this->c_0++;
 			else if (0 < $taxon->c_o_c && $taxon->c_o_c < 4)
-				$c_1_3++;
+				$this->c_1_3++;
 			else if (3 < $taxon->c_o_c && $taxon->c_o_c < 7)
-				$c_4_6++;
+				$this->c_4_6++;
 			else if (6 < $taxon->c_o_c && $taxon->c_o_c < 11)
-				$c_7_10++;
+				$this->c_7_10++;
 			
 			if ($taxon->duration == 'annual') {
-				$duration = true;
+				$this->duration = true;
 				$this->annual++;
 				if ($taxon->native == 'native') {
 					$this->native_annual++;
 				}
 			}
 			if ($taxon->duration == 'perennial') {
-				$duration = true;
+				$this->duration = true;
 				$this->perennial++;
 				if ($taxon->native == 'native') {
 					$this->native_perennial++;
 				}
 			}
 			if ($taxon->duration == 'biennial') {
-				$duration = true;
+				$this->duration = true;
 				$this->biennial++;
 				if ($taxon->native == 'native') {
 					$this->native_biennial++;
@@ -152,82 +153,82 @@ class QuadratMetrics extends Metrics {
 			}
 			switch ($taxon->physiognomy) {
 				case 'tree':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->tree++;
 					if ($taxon->native == 'native') {
-						$native_tree_c += $taxon->c_o_c;
-						$native_tree++;
+						$this->native_tree_c += $taxon->c_o_c;
+						$this->native_tree++;
 					}
 					break;
 				case 'shrub':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->shrub++;
 					if ($taxon->native == 'native') {
-						$native_shrub_c += $taxon->c_o_c;
-						$native_shrub++;
+						$this->native_shrub_c += $taxon->c_o_c;
+						$this->native_shrub++;
 					}
 					break;
 				case 'vine':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->vine++;
 					if ($taxon->native == 'native') {
-						$native_herbaceous_c += $taxon->c_o_c;
-						$native_herbaceous++;
+						$this->native_herbaceous_c += $taxon->c_o_c;
+						$this->native_herbaceous++;
 					}
 					break;
 				case 'forb':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->forb++;
 					if ($taxon->native == 'native') {
-						$native_herbaceous_c += $taxon->c_o_c;
-						$native_herbaceous++;
+						$this->native_herbaceous_c += $taxon->c_o_c;
+						$this->native_herbaceous++;
 					}
 					break;
 				case 'grass':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->grass++;
 					if ($taxon->native == 'native') {
-						$native_herbaceous_c += $taxon->c_o_c;
-						$native_herbaceous++;
+						$this->native_herbaceous_c += $taxon->c_o_c;
+						$this->native_herbaceous++;
 					}
 					break;
 				case 'sedge':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->sedge++;
 					if ($taxon->native == 'native') {
-						$native_herbaceous_c += $taxon->c_o_c;
-						$native_herbaceous++;
+						$this->native_herbaceous_c += $taxon->c_o_c;
+						$this->native_herbaceous++;
 					}
 					break;
 				case 'rush':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->rush++;
 					if ($taxon->native == 'native') {
-						$native_herbaceous_c += $taxon->c_o_c;
-						$native_herbaceous++;
+						$this->native_herbaceous_c += $taxon->c_o_c;
+						$this->native_herbaceous++;
 					}
 					break;
 				case 'fern':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->fern++;
 					if ($taxon->native == 'native') {
-						$native_herbaceous_c += $taxon->c_o_c;
-						$native_herbaceous++;
+						$this->native_herbaceous_c += $taxon->c_o_c;
+						$this->native_herbaceous++;
 					}
 					break;
 				case 'bryophyte':
-					$physiognomy = true;
+					$this->physiognomy = true;
 					$this->bryophyte++;
 					if ($taxon->native == 'native') {
-						$native_herbaceous_c += $taxon->c_o_c;
-						$native_herbaceous++;
+						$this->native_herbaceous_c += $taxon->c_o_c;
+						$this->native_herbaceous++;
 					}
 					break;
 			}
 			
 		}
 		
-		if (!$physiognomy) {
+		if (!$this->physiognomy) {
 			$this->tree = 'n/a';
 			$this->shrub = 'n/a';
 			$this->vine = 'n/a';
@@ -247,23 +248,23 @@ class QuadratMetrics extends Metrics {
 			$this->percent_fern = '';
 			$this->percent_bryophyte = '';
 		} else {
-			if ($native_tree == 0)
+			if ($this->native_tree == 0)
 				$this->native_tree_mean_c = 'n/a';
 			else
 				if ($this->native_species > 0) {
-					$this->native_tree_mean_c = round(($native_tree_c / $this->native_species), 1);
+					$this->native_tree_mean_c = round(($this->native_tree_c / $this->native_species), 1);
 				}
-			if ($native_shrub == 0)
+			if ($this->native_shrub == 0)
 				$this->native_shrub_mean_c = 'n/a';
 			else
 				if ($this->native_species > 0) {
-					$this->native_shrub_mean_c = round(($native_shrub_c / $this->native_species), 1);
+					$this->native_shrub_mean_c = round(($this->native_shrub_c / $this->native_species), 1);
 				}
-			if ( $native_herbaceous == 0 )
+			if ( $this->native_herbaceous == 0 )
 				$this->native_herbaceous_mean_c = 'n/a';
 			else
 				if ($this->native_species > 0) {
-					$this->native_herbaceous_mean_c = round(($native_herbaceous_c / $this->native_species), 1);
+					$this->native_herbaceous_mean_c = round(($this->native_herbaceous_c / $this->native_species), 1);
 				}
 			if ($this->total_species > 0) {
 				$this->percent_tree = round(100*$this->tree / $this->total_species,1);
@@ -278,7 +279,7 @@ class QuadratMetrics extends Metrics {
 			}
 		}
 		
-		if (!$duration) {
+		if (!$this->duration) {
 			$this->annual = 'n/a';
 			$this->perennial = 'n/a';
 			$this->biennial = 'n/a';
@@ -306,11 +307,11 @@ class QuadratMetrics extends Metrics {
 			}
 		}
 		
-		if ($wetness) {
+		if ($this->wetness) {
 			if ($this->total_species > 0)
-				$this->mean_wetness = round(($total_w / $this->total_species),1);
+				$this->mean_wetness = round(($this->total_w / $this->total_species),1);
 			if ($this->native_species > 0)
-				$this->native_mean_wetness = round(($native_w / $this->native_species),1);
+				$this->native_mean_wetness = round(($this->native_w / $this->native_species),1);
 		} else {
 			$this->mean_wetness = 'n/a';
 			$this->native_mean_wetness = 'n/a';
@@ -319,21 +320,21 @@ class QuadratMetrics extends Metrics {
 		$this->non_native_species = $this->total_species - $this->native_species;
 			
 		if ($this->native_species > 0) {
-			$this->native_mean_c = round(($native_c / $this->native_species),1);
+			$this->native_mean_c = round(($this->native_c / $this->native_species),1);
 			$this->native_fqi = round(($this->native_mean_c * sqrt( $this->native_species ) ), 1);
-			if ($native_coverage > 0) {
-				$this->cover_weighted_native_mean_c = round( $sum_native_c_times_coverage / $native_coverage , 1 );
+			if ($this->native_coverage > 0) {
+				$this->cover_weighted_native_mean_c = round( $this->sum_native_c_times_coverage / $this->native_coverage , 1 );
 				$this->cover_weighted_native_fqi = round( ($this->cover_weighted_native_mean_c * sqrt( $this->native_species )), 1);
 			}
 		}
 		
 		if ($this->total_species > 0) {
 		
-			$this->total_mean_c = round(($total_c / $this->total_species),1);
-			$this->percent_c_0 = round(($c_0 / $this->total_species) * 100,1);
-			$this->percent_c_1_3 = round(($c_1_3 / $this->total_species) * 100,1);
-			$this->percent_c_4_6 = round(($c_4_6 / $this->total_species) * 100,1);
-			$this->percent_c_7_10 = round(($c_7_10 / $this->total_species) * 100,1);
+			$this->total_mean_c = round(($this->total_c / $this->total_species),1);
+			$this->percent_c_0 = round(($this->c_0 / $this->total_species) * 100,1);
+			$this->percent_c_1_3 = round(($this->c_1_3 / $this->total_species) * 100,1);
+			$this->percent_c_4_6 = round(($this->c_4_6 / $this->total_species) * 100,1);
+			$this->percent_c_7_10 = round(($this->c_7_10 / $this->total_species) * 100,1);
 			
 			$this->percent_native_species = round(100*($this->native_species/$this->total_species),1);
 			$this->percent_non_native_species = round(100*($this->non_native_species/$this->total_species),1);
@@ -341,8 +342,8 @@ class QuadratMetrics extends Metrics {
 			$this->total_fqi = round(( $this->total_mean_c * sqrt( $this->total_species ) ), 1);
 			$this->adjusted_fqi = round( ( ( $this->native_mean_c / 10 ) * ( sqrt( $this->native_species ) / sqrt( $this->total_species ) ) * 100 ), 1);
 			
-			if ($total_coverage > 0) {
-				$this->cover_weighted_total_mean_c = round( $sum_total_c_times_coverage / $total_coverage , 1 );
+			if ($this->total_coverage > 0) {
+				$this->cover_weighted_total_mean_c = round( $this->sum_total_c_times_coverage / $this->total_coverage , 1 );
 				$this->cover_weighted_total_fqi = round( ($this->cover_weighted_total_mean_c * sqrt( $this->total_species )), 1);
 			}
 		}	
