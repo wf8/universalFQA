efe56748914230ceed655b28453b3447d8f0a792
wf <willfreyman@gmail.com>
2013-09-15 10:41:35 -0700

    analytics

--- views/footer.php
+++ views/footer.php
@@ -3,5 +3,13 @@
 			<p><a href="http://universalFQA.org">universalFQA.org</a> | <a href="/about">About this site</a></p>
 		</div>
 	</footer>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-44045275-1', 'universalfqa.org');
+		ga('send', 'pageview');
+	</script>
   </body>
 </html>
